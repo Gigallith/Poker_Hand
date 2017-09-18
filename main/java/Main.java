b11f94e72224fa6b619d02d73333f2ec61df6a9f
Gigallith <paul.dupont@etu.unice.fr>
2017-09-18 15:37:46 +0200

    gestion de l'entrée de la seconde main

--- main/java/Main.java
+++ main/java/Main.java
@@ -16,7 +16,6 @@ public class Main {
         String str1 = sc.nextLine();
 
         List<Card> list1 = new ArrayList<Card>();
-
         String values1[] = str1.split(" ");
         for (String i : values1) {
             Value value;
@@ -32,12 +31,31 @@ public class Main {
             Card card = new Card(value,color);
             list1.add(card);
         }
+        Hand hand1 = new Hand(list1);
 
 
 
         //Création de la main 2
         System.out.print("\nMain 2 :    ");
         String str2 = sc.nextLine();
+
+        List<Card> list2 = new ArrayList<Card>();
+        String values2[] = str1.split(" ");
+        for (String i : values2) {
+            Value value;
+            Color color;
+            //différencier le 10 du 1
+            if (i.length() > 3){
+                value = Value.find(i.substring(0,2));
+                color = Color.find(i.substring(2));
+            } else {
+                value = Value.find(i.substring(0,1));
+                color = Color.find(i.substring(1));
+            }
+            Card card = new Card(value,color);
+            list2.add(card);
+        }
+        Hand hand2 = new Hand(list2);
 
         Game game = new Game();
     }
